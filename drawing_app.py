44864b079a83d8d9899e8ca1bbdf11e7793b6e78
miwkom <166976262+miwkom@users.noreply.github.com>
2024-12-27 06:14:09 +0300

    Add files via upload

--- drawing_app.py
+++ drawing_app.py
@@ -10,16 +10,18 @@ class DrawingApp:
         self.root = root
         self.root.title("Рисовалка с сохранением в PNG")
 
-        self.image = Image.new("RGB", (600, 400), "white")
+        self.image = Image.new("RGB", (500, 400), "white")
         self.draw = ImageDraw.Draw(self.image)
 
-        self.canvas = tk.Canvas(root, width=600, height=400, bg='white')
+        self.canvas = tk.Canvas(root, width=600, height=400, bg='white', bd=0, highlightthickness=0)
         self.canvas.pack()
 
         self.pen_color = 'black'
         self.eraser_color = self.pen_color
 
         self.setup_ui()
+
+        self.setup_menu()
 
         self.last_x, self.last_y = None, None
 
@@ -32,33 +34,44 @@ class DrawingApp:
         """
         Настраивает пользовательский интерфейс с инструментами.
         """
-        control_frame = tk.Frame(self.root)
-        control_frame.pack(fill=tk.X)
+        control_frame = tk.Frame(self.root, bg='lightgray')
+        control_frame.pack(fill=tk.X, side=tk.BOTTOM)
 
         clear_button = tk.Button(control_frame, text="Очистить", command=self.clear_canvas)
-        clear_button.pack(side=tk.LEFT)
+        clear_button.pack(side=tk.LEFT, padx=1)
 
         color_button = tk.Button(control_frame, text="Выбрать цвет", command=self.choose_color)
-        color_button.pack(side=tk.LEFT)
+        color_button.pack(side=tk.LEFT, padx=1)
 
         self.selected_color = tk.Label(control_frame, width=2, bg=self.pen_color)
-        self.selected_color.pack(side=tk.LEFT)
+        self.selected_color.pack(side=tk.LEFT, padx=1)
 
         brush_button = tk.Button(control_frame, text="Кисть", command=self.use_brush)
-        brush_button.pack(side=tk.LEFT)
+        brush_button.pack(side=tk.LEFT, padx=1)
 
         eraser_button = tk.Button(control_frame, text="Ластик", command=self.use_eraser)
-        eraser_button.pack(side=tk.LEFT)
-
-        save_button = tk.Button(control_frame, text="Сохранить", command=self.save_image)
-        save_button.pack(side=tk.LEFT)
+        eraser_button.pack(side=tk.LEFT, padx=1)
 
         sizes = range(1,11)
         self.brush_size_var = tk.StringVar()
         self.brush_size_var.set(sizes[0])
         self.brush_size_menu = tk.OptionMenu(control_frame, self.brush_size_var, *sizes,
                                              command=self.update_brush_size)
-        self.brush_size_menu.pack(side=tk.LEFT)
+        self.brush_size_menu.pack(side=tk.LEFT, padx=1)
+
+    def setup_menu(self):
+        """
+        Настраивает верхнее меню приложения
+        """
+        self.main_menu = tk.Menu(self.root)
+        self.root.config(menu=self.main_menu, bg='lightgray')
+
+        file_menu = tk.Menu(self.main_menu, tearoff=0)
+
+        self.main_menu.add_cascade(label="Файл", menu=file_menu)
+        file_menu.add_command(label="Создать новое изображение", command=self.new_image)
+        file_menu.add_separator()
+        file_menu.add_command(label="Сохранить", command=self.save_image)
 
     def bind_shortcuts(self):
         """
@@ -132,6 +145,27 @@ class DrawingApp:
         color = self.image.getpixel((x, y))
         self.pen_color = '#{:02x}{:02x}{:02x}'.format(*color)
         self.selected_color.config(bg=self.pen_color)
+
+    def new_image(self):
+        """
+        Создает новое изображение и настраивает размеры приложения под его размер.
+        """
+        x = tk.simpledialog.askinteger("Создать новое изображение", "Введите целое число для обозначения ширины:",
+                                       initialvalue=600, minvalue=100, maxvalue=1500, parent=self.root)
+        if x is None:
+            return
+
+        y = tk.simpledialog.askinteger("Создать новое изображение", "Введите целое число для обозначения высоты:",
+                                       initialvalue=400, minvalue=100, maxvalue=800, parent=self.root)
+        if y is None:
+            return
+
+        if x and y:
+            self.image = Image.new("RGB", (x, y), "white")
+            self.draw = ImageDraw.Draw(self.image)
+            self.canvas.config(width=x, height=y)
+            self.clear_canvas()
+
 
     def save_image(self, event=None):
         """
